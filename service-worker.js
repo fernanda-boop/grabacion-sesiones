e24aa525433679554a0e41f23c43bde10b936ee8
fernanda-boop <lic@xn--psiclogaagustinavelasco-8ic.com>
2026-01-27 05:16:40 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'grabadora-clinica-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Instalación del Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('✅ Cache abierto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activación del Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('🗑️ Eliminando cache antigua:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptar peticiones
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
